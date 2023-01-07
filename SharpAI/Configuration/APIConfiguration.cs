cd6523d4f46d4baae10c1730ff421e2c5709ba62
Kylnic28 <nicoletkyl@gmail.com>
2023-01-07 12:59:23 +0100

    fix mistake

--- SharpAI/Configuration/APIConfiguration.cs
+++ SharpAI/Configuration/APIConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public class APIConfiguration
     {
-        public string? Key { get; set; } = "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";
+        public string? Key { get; set; }
         public string? Organization { get; set; }
     }
 }
